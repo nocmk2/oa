4a01ca502c8ab1ae6445fb823a7f30e7c4a16aef
davidcai <376462191@qq.com>
2015-03-10 21:13:13 +0800

    engprj添加，编辑

--- app/controllers/engproj.js
+++ app/controllers/engproj.js
@@ -9,16 +9,56 @@ module.exports = function (app) {
             if (err) {
                 engprojs = [];
             }
-            console.log("enprojs: ");
-            console.log(util.inspect(engprojs,{depth:3}));
-            console.log("engprojs[0].basicInfo.serialno: ");
-            console.log(engprojs[0].basicInfo.serialno);
             res.render('engineeringproj', {
                 title: '工程公司项目管理',
                 user: req.session.user,
                 engprojs: engprojs,
                 success: req.flash('success').toString(),
                 error: req.flash('error').toString()
+            });
+        });
+    });
+
+    //保存项目
+    app.post('/engineeringproj/save',checkLogin,function(req,res){
+        var proj = req.body.proj;
+        EngprojService.save(proj,function (err, proj) {
+            if (err) {
+                return res.send({
+                    success:false,
+                    msg:"数据存储出错"
+                });
+            }
+            return res.send({
+                success:true
+            });
+        });
+    });
+
+    //获取项目信息
+    app.post('/engineeringproj/getById',checkLogin,function(req,res){
+        var id = req.body.id;
+        EngprojService.getById(id,function(err,engproj){
+            if(err){
+                console.log(err);
+                req.flash('error', err);
+                res.send({engproj:null});
+            }
+            return res.send(engproj);
+        });
+    });
+
+    //编辑项目信息
+    app.post('/engineeringproj/edit',checkLogin,function(req,res){
+        var proj = req.body.proj;
+        EngprojService.updateOne(proj,function (err, nUpdated) {
+            if (err) {
+                return res.send({
+                    success:false
+                });
+            }
+            return res.send({
+                success:true
             });
         });
     });
--- app/models/services/engproj.js
+++ app/models/services/engproj.js
@@ -2,9 +2,44 @@ var Engproj = require('../models/engproj');
 
 var engprojService = {};
 
+//获取所有项目
 engprojService.getAll = function(callback){
     var engprojs = Engproj.find({});
     engprojs.exec(callback);
+};
+
+//保存项目
+engprojService.save = function(engproj,callback){
+    var engproj = new Engproj(engproj);
+    engproj.save(function(err,engprojSaved){
+        if(err) {
+            callback(err);
+        }else{
+            callback(null,engprojSaved);
+        }
+    });
+};
+
+//依据ID获取项目
+engprojService.getById = function(id,callback){
+    Engproj.findById(id,function(err,engproj){
+        if(err){
+            callback(err);
+        }else{
+            callback(null,engproj);
+        }
+    });
+};
+
+//更新项目
+engprojService.updateOne = function(engproj,callback){
+    Engproj.findByIdAndUpdate(engproj._id,engproj,function(err,nUpdated){
+        if(err){
+            callback(err);
+        }else{
+            callback(null,nUpdated);
+        }
+    });
 };
 
 module.exports = engprojService;
